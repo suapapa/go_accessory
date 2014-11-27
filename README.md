82d4f3a11c2924f50775b4fc775575276078af7f
Homin Lee <homin.lee@suapapa.net>
2014-11-27 20:20:42 +0900

    Add documentation

---
+++ README.md
@@ -0,0 +1,6 @@
+# Open Accessory Protocol
+
+* [Version 1.0](https://source.android.com/accessories/aoa.html)
+* [Version 2.0](https://source.android.com/accessories/aoa2.html)
+
+
---
+++ doc.go
@@ -0,0 +1,8 @@
+// Copyright 2014, Homin Lee <homin.lee@suapapa.net>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+  Package accessory is for handle Android Open Accessory
+*/
+package accessory
